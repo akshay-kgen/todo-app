62f957a86f7638dd65b4d3700946ef2fe300e544
Akshay <akshay.mundra.gkmit@kgen.io>
2025-02-05 10:54:45 +0530

    fix: add user exist check for register

--- services/auth.service.go
+++ services/auth.service.go
@@ -23,6 +23,18 @@ func NewAuthService(repo *repo.UserRepo) *AuthService {
 }
 
 func (s *AuthService) Register(ctx context.Context, user *models.UserModel) *helpers.CustomError {
+
+	// email should be unique for the user
+	userExist, err := s.userRepo.GetUserByEmail(user.Email)
+	if err != nil {
+		log.Printf("failed to query user by email: %v", err)
+		return helpers.NewCustomError(fmt.Errorf("internal error while checking user existence: %v", err), "500")
+	}
+
+	if userExist != nil {
+		return helpers.NewCustomError(fmt.Errorf("user already exists"), "409")
+	}
+
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
 		log.Printf("failed to hash password: %v", err)
